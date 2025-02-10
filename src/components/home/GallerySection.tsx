bdd275f7082276720e0d22ef055e772f57244158
kelvintryanto <kelvin.tryanto@gmail.com>
2025-02-10 17:55:50 +0700

    correction color, navbar responsive, service section

--- src/components/home/GallerySection.tsx
+++ src/components/home/GallerySection.tsx
@@ -1,15 +1,15 @@
 const GallerySection = () => {
   return (
-    <div className="w-full h-screen bg-gradient-to-b from-violet-100 from-5% to-violet-300 to-90%">
+    <div className="w-full h-screen bg-gradient-to-b from-violet-800/80 to-violet-800">
       <div className="flex flex-col items-center justify-center p-10 relative">
-        <h1 className="text-3xl font-bold">
+        <h1 className="text-3xl font-bold text-orange-500">
           {/* Layanan*/}
           Gallery
         </h1>
 
-        <div className="absolute top-0 left-0 w-full -z-10">
+        <div className="absolute top-0 left-0 w-full z-10">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#273036" fillOpacity="0.2" d="M0,320L48,309.3C96,299,192,277,288,229.3C384,181,480,107,576,112C672,117,768,203,864,218.7C960,235,1056,181,1152,144C1248,107,1344,85,1392,74.7L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+            <path fill="#273036" fillOpacity="0.1" d="M0,320L48,309.3C96,299,192,277,288,229.3C384,181,480,107,576,112C672,117,768,203,864,218.7C960,235,1056,181,1152,144C1248,107,1344,85,1392,74.7L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
           </svg>
         </div>
       </div>
