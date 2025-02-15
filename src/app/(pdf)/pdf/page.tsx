93d1361ee7792d565ae2f14183e8b616c36ff85b
raga <ragaramadhannur2017@gmail.com>
2025-02-15 12:47:22 +0700

    feat:pdf simple

---
+++ src/app/(pdf)/pdf/page.tsx
@@ -0,0 +1,10 @@
+import InvoiceForm from "@/components/invoice-form"
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-background">
+      <InvoiceForm />
+    </main>
+  )
+}
+
