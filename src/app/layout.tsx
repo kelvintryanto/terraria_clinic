8ce25ac08335bd2d1e638f42bc5e1ef0e92d4ed0
Ben <BenZeta@Bens-Mac.local>
2025-02-17 22:54:06 +0700

    fix: double footer and can't access public route when not logged in

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,17 @@
-import type { Metadata } from "next";
-import "./globals.css";
-<<<<<<< HEAD
-=======
-import Footer from "@/components/Footer ";
-import { NavBar } from "@/components/navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
->>>>>>> 93d1361ee7792d565ae2f14183e8b616c36ff85b
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "TerrariaVet | Rumah Terraria",
-  description: "Merawat dengan Cinta, Menjaga dengan Keahlian",
-  icons: "/logo_white.png",
+  title: 'TerrariaVet | Rumah Terraria',
+  description: 'Merawat dengan Cinta, Menjaga dengan Keahlian',
+  icons: '/logo_white.png',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>{children}</body>
