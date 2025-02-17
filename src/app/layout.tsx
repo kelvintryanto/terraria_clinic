8e3d6a84432321282637e9bb1a99e73b5913a7bc
kelvintryanto <kelvin.tryanto@gmail.com>
2025-02-17 22:53:26 +0700

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-<<<<<<< HEAD
-=======
-import Footer from "@/components/Footer ";
-import { NavBar } from "@/components/navbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
->>>>>>> 93d1361ee7792d565ae2f14183e8b616c36ff85b
 
 export const metadata: Metadata = {
   title: "TerrariaVet | Rumah Terraria",
